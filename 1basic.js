c223197c9a154b9acabe0e12d3ccc12fa5a99218
Rahul Kumar Sah <rahulrks9805@gmail.com>
2024-10-11 04:51:54 +0530

    Basics of JavaScript

---
+++ 1basic.js
@@ -0,0 +1,14 @@
+var a = 10;
+console.log(a);
+var a = 11;
+console.log(a);
+
+
+let b = 24;
+console.log(b);
+b = 45;
+console.log(b);
+
+
+const pi = 3.14;
+console.log(pi);
---
+++ 2string.js
@@ -0,0 +1,6 @@
+let firstName = "Rahul";
+console.log(firstName[3]);
+
+console.log(firstName.length);            // length is not a function
+console.log(firstName[firstName.length-1]);
+console.log(firstName[-1]);      // undefined
